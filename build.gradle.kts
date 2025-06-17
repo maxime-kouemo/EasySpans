b8dac4037a385037a603d77cd9f251f403703d3c
mamboa <koumaxim@yahoo.fr>
2025-06-17 13:44:54 -0400

    attempt to fix the release build

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,20 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/maxime-kouemo/easyspans")
             credentials {
-                username = (project.findProperty("githubUsername") ?: System.getenv("githubUsername")).toString()
-                password = (project.findProperty("githubToken") ?: System.getenv("githubToken")).toString()
+                username = (project.findProperty("githubUsername")
+                    ?: System.getenv("githubUsername")).toString()
+                password =
+                    (project.findProperty("githubToken") ?: System.getenv("githubToken")).toString()
+            }
+        }
+        maven {
+            name = "JitPack"
+            url = uri("https://jitpack.io")
+            credentials {
+                username = ((project.findProperty("jitpackUsername")
+                    ?: System.getenv("jitpackUsername")).toString())
+                password = (project.findProperty("jitpackToken")
+                    ?: System.getenv("jitpackToken")).toString()
             }
         }
     }
